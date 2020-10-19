95d2ffd6d45e9384e386c141c3a1bd0ad6bf5668
André Silva <andre00nogueira@gmail.com>
2020-10-19 22:53:12 +0100

    Initial Commit.

---
+++ app/src/main/java/io/github/andre00nogueira/daeprojectclient/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.andre00nogueira.daeprojectclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
